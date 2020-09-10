d63d37ad27638cae1f8b69eb1fb08a7bd3819ce1
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-09-10 21:35:00 +0530

    light direction logic updated

--- script.js
+++ script.js
@@ -8,11 +8,16 @@ let dist = document.getElementById('distance');
 let sliders = document.querySelectorAll('.slider');
 console.log(sliders);
 
+let left = '';
+let right = '-';
+let Top = '';
+let bottom = '-';
+
 function updateValues() {
 	box.style.width = `${size.value}px`;
 	box.style.height = `${size.value}px`;
 	box.style.borderRadius = `${rad.value}px`;
-	box.style.boxShadow = `${dist.value}px ${dist.value}px ${blur.value}px #cbced1, -${dist.value}px -${dist.value}px ${blur.value}px #fff`;
+	box.style.boxShadow = `${left}${dist.value}px ${Top}${dist.value}px ${blur.value}px #cbced1, ${right}${dist.value}px ${bottom}${dist.value}px ${blur.value}px #fff`;
 }
 
 sliders.forEach(e => {
@@ -20,3 +25,44 @@ sliders.forEach(e => {
 });
 
 updateValues();
+
+let tl = document.getElementById('light-top-left');
+let tr = document.getElementById('light-top-right');
+let bl = document.getElementById('light-bottom-left');
+let br = document.getElementById('light-bottom-right');
+
+tl.addEventListener('click', () => {
+	left = '';
+	right = '-';
+	Top = '';
+	bottom = '-';
+	updateValues();
+	console.log('top left clicked');
+});
+
+tr.addEventListener('click', () => {
+	left = '-';
+	right = '';
+	Top = '';
+	bottom = '-';
+	updateValues();
+	console.log('top right clicked');
+});
+
+bl.addEventListener('click', () => {
+	left = '';
+	right = '-';
+	Top = '-';
+	bottom = '';
+	updateValues();
+	console.log('bottom left clicked');
+});
+
+br.addEventListener('click', () => {
+	left = '-';
+	right = '';
+	Top = '-';
+	bottom = '';
+	updateValues();
+	console.log('bottom right clicked');
+});
