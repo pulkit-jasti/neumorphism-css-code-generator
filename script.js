f25a0c4ad9b1a012e3b4b3b46dbce97da360ab50
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-09-10 21:06:01 +0530

    title,icon and minor styling updates

--- script.js
+++ script.js
@@ -20,19 +20,3 @@ sliders.forEach(e => {
 });
 
 updateValues();
-
-/*
-size.addEventListener('input', function () {
-	box.style.width = `${this.value}px`;
-	box.style.height = `${this.value}px`;
-});
-
-rad.addEventListener('input', function () {
-	box.style.borderRadius = `${this.value}px`;
-});
-
-dist.addEventListener('input', function () {
-	box.style.boxShadow = `${this.value}px ${this.value}px 20px #cbced1, -${this.value}px -${this.value}px 20px #fff`;
-	console.log(this.value);
-});
-*/
